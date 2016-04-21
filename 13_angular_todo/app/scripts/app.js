a4372bb34654e3fd32a3a937b1d9b0067e8211cf
JonDai <jondai@aliyun.com>
2016-04-21 15:37:46 +0800

    add 13_angular_todo

---
+++ 13_angular_todo/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name 13AngularTodoApp
+ * @description
+ * # 13AngularTodoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('13AngularTodoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ 13_angular_todo/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 13AngularTodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the 13AngularTodoApp
+ */
+angular.module('13AngularTodoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
